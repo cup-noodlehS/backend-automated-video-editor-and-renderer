44037e7e9d142beff04acae39afba193ea14c680
Sheldon Sagrado <sheldon.bposeats@gmail.com>
2024-10-08 15:48:47 +0800

    add organization and auth apps

--- video/models.py
+++ video/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class File(models.Model):
+    name = models.CharField(max_length=255)
+    url = models.URLField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Font(models.Model):
+    name = models.CharField(max_length=255)
+    file = models.ForeignKey(File, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class AeVersion(models.Model):
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
