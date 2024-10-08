f52b098f485b42a8d3e6b1a19ef6cb26b16db421
cup-noodlehS <sheldonarthursagrado@gmail.com>
2024-10-08 20:14:54 +0800

    add user model admin

--- tranches/accounts/admin.py
+++ tranches/accounts/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import CustomUser
 
-# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'is_admin', 'is_staff', 'is_active', 'date_joined')
+    search_fields = ('username', 'email')
+    list_filter = ('is_admin', 'is_staff', 'is_active', 'date_joined')
+    ordering = ('-date_joined',)
+    readonly_fields = ('date_joined',)
+
+    fieldsets = (
+        (None, {
+            'fields': ('first_name', 'last_name', 'username', 'email', 'password')
+        }),
+        ('Permissions', {
+            'fields': ('is_admin', 'is_superuser', 'is_staff', 'is_active')
+        }),
+        ('Important Dates', {
+            'fields': ('last_login', 'date_joined')
+        }),
+    )
