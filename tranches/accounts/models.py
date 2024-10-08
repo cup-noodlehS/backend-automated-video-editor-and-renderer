af1ef3c428709392befd70592263584f2ae9f545
cup-noodlehS <sheldonarthursagrado@gmail.com>
2024-10-08 23:34:49 +0800

    install djangorestframework-simplejwt

--- tranches/accounts/models.py
+++ tranches/accounts/models.py
@@ -2,6 +2,14 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
+    '''
+    **Required Fields:**
+    - first_name: CharField to store the first name of the user.
+    - last_name: CharField to store the last name of the user.
+    - password: CharField to store the password of the user.
+    - email: EmailField to store the email address of the user.
+    - is_admin: BooleanField to store whether the user is an admin or not.
+    '''
     is_admin = models.BooleanField(default=False)
 
     def __str__(self):
