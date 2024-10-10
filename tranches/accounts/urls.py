ac8ffdff41c08b8623c383f6b35b697ed2a4d12f
cup-noodlehS <sheldonarthursagrado@gmail.com>
2024-10-10 22:10:20 +0800

    add organization api endpoint urls

--- tranches/accounts/urls.py
+++ tranches/accounts/urls.py
@@ -3,8 +3,8 @@ from .views import RegisterView, CustomTokenObtainPairView, UserProfileView
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
-    path('api/register/', RegisterView.as_view(), name='register'),
-    path('api/login/', CustomTokenObtainPairView.as_view(), name='login'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/profile/', UserProfileView.as_view(), name='user_profile'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', CustomTokenObtainPairView.as_view(), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('profile/', UserProfileView.as_view(), name='user_profile'),
 ]
