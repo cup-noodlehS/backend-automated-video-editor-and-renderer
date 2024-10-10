54a8c628b9e00c4288b06eb0d953e8994477f4d9
cup-noodlehS <sheldonarthursagrado@gmail.com>
2024-10-10 22:11:53 +0800

    add api endpoints

--- tranches/organization/views.py
+++ tranches/organization/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from tranches.utils.generic_api import GenericView
+from organization.models import Organization, OrganizationMember, Credits
+from organization.serializers import OrganizationSerializer, OrganizationMemberSerializer, CreditsSerializer
 
-# Create your views here.
+
+class OrganizationView(GenericView):
+    queryset = Organization.objects.filter(archived=False)
+    serializer_class = OrganizationSerializer
+
+
+class OrganizationMemberView(GenericView):
+    queryset = OrganizationMember.objects.all()
+    serializer_class = OrganizationMemberSerializer
+
+
+class CreditsView(GenericView):
+    queryset = Credits.objects.all()
+    serializer_class = CreditsSerializer
