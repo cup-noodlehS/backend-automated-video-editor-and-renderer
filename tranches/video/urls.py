ce3a954a2fb07ff7042ffe11f3c1b6a3618a8b0e
cup-noodlehS <sheldonarthursagrado@gmail.com>
2024-10-11 10:17:05 +0800

    add video templates urls

--- tranches/video/urls.py
+++ tranches/video/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from video.views import FontView, AeVersionView
+from video.views import FontView, AeVersionView, VideoTemplateView, DetailedVideoTemplateView, StaticLayerView, DynamicLayerView
 
 
 urlpatterns = [
@@ -7,4 +7,10 @@ urlpatterns = [
     path('fonts/<int:pk>/', FontView.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='font-detail'),
     path('ae-versions/', AeVersionView.as_view({'get': 'list', 'post': 'create'}), name='ae-version-list'),
     path('ae-versions/<int:pk>/', AeVersionView.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='ae-version-detail'),
+    path('video-templates/', VideoTemplateView.as_view({'get': 'list', 'post': 'create'}), name='video-template-list'),
+    path('video-templates/<int:pk>/', DetailedVideoTemplateView.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='video-template-detail'),
+    path('static-layers/', StaticLayerView.as_view({'get': 'list', 'post': 'create'}), name='static-layer-list'),
+    path('static-layers/<int:pk>/', StaticLayerView.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='static-layer-detail'),
+    path('dynamic-layers/', DynamicLayerView.as_view({'get': 'list', 'post': 'create'}), name='dynamic-layer-list'),
+    path('dynamic-layers/<int:pk>/', DynamicLayerView.as_view({'get': 'retrieve', 'put': 'update', ' delete': 'destroy'}), name='dynamic-layer-detail'),
 ]
