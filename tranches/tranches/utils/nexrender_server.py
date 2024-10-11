f417e9d27fe3400fc23263300db65f2716cb7b9a
cup-noodlehS <sheldonarthursagrado@gmail.com>
2024-10-11 09:27:26 +0800

    add render video function

--- tranches/tranches/utils/nexrender_server.py
+++ tranches/tranches/utils/nexrender_server.py
@@ -2,24 +2,19 @@ import requests
 from django.conf import settings
 
 
+nexrender_api_url = getattr(settings, 'NEXRENDER_API_URL', None)
+
 class FontInstallationError(Exception):
+    pass
+
+class VideoRenderingError(Exception):
     pass
 
 def install_font(url):
     """
-    Sends a request to the Nexrender API to install a font.
-    
-    Args:
-        url (str): The URL of the font to install
-        
-    Returns:
-        dict: The response from the API
-        
-    Raises:
-        FontInstallationError: If the font installation fails
+    Install a font from a URL to the Nexrender server
     """
     print("Installing font")
-    nexrender_api_url = getattr(settings, 'NEXRENDER_API_URL', None)
     if not nexrender_api_url:
         raise FontInstallationError("NEXRENDER_API_URL is not set in environment variables")
     
@@ -39,3 +34,37 @@ def install_font(url):
         
     except requests.RequestException as e:
         raise FontInstallationError(f"Failed to connect to Nexrender API: {str(e)}")
+
+
+def render_video(template_uri, composition_name, assets, priority=0):
+    """
+    Render a video using the Nexrender server. 
+    **Returns** the **jobId** of the rendering job
+    """
+    print("Rendering video")
+    if not nexrender_api_url:
+        raise VideoRenderingError("NEXRENDER_API_URL is not set in environment variables")
+    
+    render_endpoint = f"{nexrender_api_url.rstrip('/')}/create-render"
+
+    try:
+        response = requests.post(
+            render_endpoint,
+            json={
+                'template_uri': template_uri,
+                'composition_name': composition_name,
+                'assets': assets,
+                'priority': priority
+            },
+            timeout=60
+        )
+
+        if response.status_code != 200:
+            raise VideoRenderingError(f"Video rendering failed with status code {response.status_code}: {response.text}")
+        
+        return response.json().get('jobId')
+    
+    except requests.RequestException as e:
+        raise VideoRenderingError(f"Failed to connect to Nexrender API: {str(e)}")
+
+# def get_render_job_details(job_id)
